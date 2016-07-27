cb7921b1043ceeb606b66b133015544a9dfa14c6
Szabo Adam <szaboadam01@hotmail.com>
2016-07-27 13:31:09 +0000

    felv

--- webapp/controller/felvetelDetail.controller.js
+++ webapp/controller/felvetelDetail.controller.js
@@ -11,6 +11,92 @@ sap.ui.define([
 
 	return Controller.extend("com.g4s.controller.felvetelDetail", {
 
+		onInit: function() {
+			var oViewModel = new JSONModel({
+				busy: false,
+				delay: 0
+			});
+
+			sap.ui.core.UIComponent.getRouterFor(this).getRoute("felvetelDetail").attachPatternMatched(this._onObjectMatched, this);
+			this.getView().setModel(oViewModel, "felvetelDetailView");
+			this.getOwnerComponent().getModel().metadataLoaded().then(this._onMetadataLoaded.bind(this));
+			//var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			//oRouter.getRoute("leadasDetail").attachPatternMatched(this._onObjectMatched, this);
+		},
+		_onObjectMatched: function(oEvent) {
+			/*this.getView().bindElement({
+				path: "/" + oEvent.getParameter("arguments").invoicePath,
+				model: "Address"
+			});*/
+
+			var addressPath = oEvent.getParameter("arguments").addressPath;
+			this.getView().getModel().metadataLoaded().then(function() {
+				var sObjectPath = this.getView().getModel().createKey("Felvet", {
+					Id: addressPath
+				});
+				this._bindView("/" + sObjectPath);
+			}.bind(this));
+
+		},
+
+		_bindView: function(sObjectPath) {
+			// Set busy indicator during view binding
+			var oViewModel = this.getView().getModel();
+
+			// If the view was not bound yet its not busy, only if the binding requests data it is set to busy again
+			oViewModel.setProperty("/busy", false);
+
+			this.getView().bindElement({
+				path: sObjectPath,
+				events: {
+					change: this._onBindingChange.bind(this),
+					dataRequested: function() {
+						oViewModel.setProperty("/busy", true);
+					},
+					dataReceived: function() {
+						oViewModel.setProperty("/busy", false);
+					}
+				}
+			});
+		},
+
+		_onBindingChange: function() {
+			var oView = this.getView(),
+				oElementBinding = oView.getElementBinding();
+
+			// No data for the binding
+			if (!oElementBinding.getBoundContext()) {
+				this.getRouter().getTargets().display("detailObjectNotFound");
+				// if object could not be found, the selection in the master list
+				// does not make sense anymore.
+				this.getOwnerComponent().oListSelector.clearMasterListSelection();
+				return;
+			}
+			var sPath = oElementBinding.getPath(),
+				//oResourceBundle = this.getView().getResourceBundle(),
+				oObject = oView.getModel().getObject(sPath),
+				Id = oObject.Id,
+				oViewModel = this.getView().getModel();
+
+			//	this.getOwnerComponent().oListSelector.selectAListItem(sPath);
+
+		},
+
+		_onMetadataLoaded: function() {
+			// Store original busy indicator delay for the detail view
+			var iOriginalViewBusyDelay = this.getView().getBusyIndicatorDelay(),
+				oViewModel = this.getView().getModel("felvetelDetailView");
+
+			// Make sure busy indicator is displayed immediately when
+			// detail view is displayed for the first time
+			//oViewModel.setProperty("/delay", 0);
+
+			// Binding the view will set it to not busy - so the view is always busy if it is not bound
+			//oViewModel.setProperty("/busy", true);
+			// Restore original busy indicator delay for the detail view
+			//oViewModel.setProperty("/delay", iOriginalViewBusyDelay);
+		},
+
 		onBeforeRendering: function() { // binding model synchronisation
 			//this.onBeforeShow();
 			window.globalleadasDetail = this;
@@ -24,7 +110,7 @@ sap.ui.define([
 
 					var myView = globalleadasDetail.getView();
 					//var model = sap.ui.getCore().getModel();
-					//ez itt szar
+					//ez itt szard
 					var oElementBinding = myView.getElementBinding();
 					var path = oElementBinding.getBoundContext().getPath();
 					var object = myView.getModel().getObject(path);
